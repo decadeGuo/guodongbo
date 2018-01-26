f93d4b28a750005800f8b33f656e05fdac30fd10
guodongbo <guodongbo@tbkt.cn>
2018-01-26 14:15:08 +0800

    Signed-off-by: guodongbo <guodongbo@tbkt.cn>

--- graduation_design/other/views.py
+++ graduation_design/other/views.py
@@ -22,4 +22,12 @@ def user_info(request):
     :param request:
     :return:
     """
-    return render(request,'others/user_info/user_info.html')
+
+
+
+    user = request.user
+    sex = u'女' if user.sex == 1 else u'男'
+    adress = user.adress if user.adress else u'暂无'
+    data = dict(id=user.id,name=user.first_name,sex=sex,phone=user.phone,part=user.depart,position=user.position,
+                level=user.user_type,idcard=user.id_card ,adress=adress)
+    return render(request,'others/user_info/user_info.html',context=data)

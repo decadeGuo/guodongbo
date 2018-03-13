5eeba6e8fc3f61d15afb05dd405cb08cfe3da7cd
guodongbo <guodongbo@tbkt.cn>
2018-03-13 17:30:23 +0800

    Signed-off-by: guodongbo <guodongbo@tbkt.cn>

--- graduation_design/meeting/urls.py
+++ graduation_design/meeting/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^meeting/apply/$',views.meeting_apply),     # 教室申请
     url(r'^applying/$', views.user_meeting_apply),  # 处理教室申请
     url(r'^apply/res/$', views.meeting_apply_res),  # 教室申请结果页面
+    url(r'^meeting/apply/ing/$', views.meeting_applying),  # 申请进度页
+    url(r'^meeting/apply/logs/$', views.meeting_logs),  # 申请日志
+    url(r'^meeting/apply/shenpi/$', views.shenpi),  # 教室审批页
     # url(r'^user/manage/update/$',views.user_manage_update),     # 信息更改
     # url(r'^ziyuan/$',views.ziyuan),     # 资源管理页面
     # url(r'^car/manage/$',views.car_manage),     # 资源管理页面
